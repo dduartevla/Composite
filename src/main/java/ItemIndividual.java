5a57bba371e514b2093848857959c484ae706edb
dduartevla <deboraduarte@ice.ufjf.br>
2023-10-12 14:11:23 -0300

    Cria a classe ItemIndividual

--- src/main/java/ItemIndividual.java
+++ src/main/java/ItemIndividual.java
@@ -1,2 +1,25 @@
-public class ItemIndividual {
+public class ItemIndividual extends Item{
+
+    private double peso;
+
+    public ItemIndividual(String nome, double valor, double peso){
+        super(nome,valor);
+        this.peso=peso;
+    }
+
+
+    @Override
+    public double getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+
+    public String getConteudoInventario(){
+        return "Item: " + this.getNome()
+                + "Valor: " + this.getValor()
+                + "Peso: " + this.peso + "\n";
+    }
 }
