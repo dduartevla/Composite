b00c7d6a87fc432bec032caa384c1c4fdb47e103
dduartevla <deboraduarte@ice.ufjf.br>
2023-10-12 13:51:17 -0300

    Cria a classe Item

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,2 +1,20 @@
-public class Item {
+public abstract class Item {
+
+    private String nome;
+    private double valor;
+
+    public Item(String nome, double valor){
+        this.nome= nome;
+        this.valor=valor;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public double getValor(){
+        return this.valor;
+    }
+
+    public abstract double getPeso();
 }
