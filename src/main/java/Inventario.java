92f7c2ba7ae7f74344a3f61107b8ca2c65023f17
dduartevla <deboraduarte@ice.ufjf.br>
2023-10-12 14:26:03 -0300

    Cria a classe Inventario

--- src/main/java/Inventario.java
+++ src/main/java/Inventario.java
@@ -1,2 +1,15 @@
 public class Inventario {
+
+    private Item item;
+
+    public void setConteudoInventario(Item item){
+        this.item=item;
+    }
+
+    public String getConteudoDoInventario(){
+        if (this.item == null){
+            throw new NullPointerException("Inventário está vazio");
+        }
+        return this.item.getConteudoInventario();
+    }
 }
